004dc9164deeddffc4cebddeadff09486de2a85b
MaximusMixik <122988460+MaximusMixik@users.noreply.github.com>
2023-02-03 18:38:21 +0200

    Add files via upload

---
+++ about_us/js/script.js
@@ -0,0 +1,35 @@
+
+//burger
+let header__burger = document.querySelectorAll(".header__burger,.header__link");
+let header__menu = document.querySelector(".header__menu");
+let back = document.querySelector("body");
+
+header__burger.forEach(function (item) {
+  item.onclick = function () {
+    item.classList.toggle("active");
+    header__menu.classList.toggle("active");
+    back.classList.toggle("lock");
+  };
+});
+
+// acordeon
+$(document).ready(function () {
+  $(".block__title").click(function (event) {
+    // ефект аккордеона(только одна вкладка может быть открыта)
+    if ($(".block").hasClass("one")) {
+      $(".block__title").not($(this)).removeClass("active");
+      $(".block__text").not($(this).next()).slideUp(300);
+    }
+    // закоментироваать участок чтобы убрать этот эфект(либо убрать класс "one" в html )
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
+
+
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+});
